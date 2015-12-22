ab31e3a175e9514bc79e875f11e6569ad70f7fd6
AuTony <codingwork2016@gmail.com>
2015-12-22 16:05:40 +1100

    find specific record(s)

--- MongoTest/Program.cs
+++ MongoTest/Program.cs
@@ -18,9 +18,20 @@ namespace MongoTest
             // get record count from database
             var count = _collection.Count(new BsonDocument());
             // pull down everything from database by using async method
-            _collection.Find(new BsonDocument()).ForEachAsync(d => Console.WriteLine(d));
-            // update record
-
+            //_collection.Find(new BsonDocument()).ForEachAsync(d => Console.WriteLine(d));
+            // find record
+            var doc = _collection.Find(new BsonDocument()).FirstOrDefault();
+            Console.WriteLine(doc.ToString());
+            Console.WriteLine("====================================================================");
+            // find a specific record
+            var _filter = Builders<BsonDocument>.Filter.Eq("restaurant_id", "40356151");
+            doc = _collection.Find(_filter).First();
+            Console.WriteLine(doc.ToString());
+            Console.WriteLine("====================================================================");
+            // find specific record sets
+            _filter = Builders<BsonDocument>.Filter.Gt("grades.score", 38);
+            _collection.Find(_filter).ForEachAsync(d => Console.WriteLine(d));            
+            Console.WriteLine("====================================================================");
             Console.WriteLine(count);
             Console.ReadKey();
         }
